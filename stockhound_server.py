e209f081e8a14436ddee3ec3f55b94e0e2ea6266
Samuel P. Gillispie II <spgill@vt.edu>
2017-05-02 14:02:09 -0400

    Added simple auth gateway to redirection

--- stockhound_server.py
+++ stockhound_server.py
@@ -1,4 +1,5 @@
 import datetime
+import functools
 import os
 import re
 import secrets
@@ -35,9 +36,32 @@ def get_article(s):
 
     return False
 
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'guest' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return flask.Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @functools.wraps(f)
+    def decorated(*args, **kwargs):
+        auth = flask.request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 
 # Index redirect
 @app.route('/')
+@requires_auth
 def index_redirect():
     return flask.redirect('/html/index.html')
 
