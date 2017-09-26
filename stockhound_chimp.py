7feb85aa04b63dd5fd951232b7dd4161227f3abb
Samuel P. Gillispie II <samuel@spgill.me>
2017-09-26 10:49:05 -0500

    Beautification

--- stockhound_chimp.py
+++ stockhound_chimp.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
                     template='notify',
                     context={
                         'ticket': ticket,
-                        'level': level
+                        'level': level,
                     }
                 )
 
